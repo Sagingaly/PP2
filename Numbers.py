1ad32c05ce74d910bc9e2c119978fe758f61813f
Sagingaly <123260920+Sagingaly@users.noreply.github.com>
2023-01-21 23:46:10 +0600

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,9 @@
+x=5
+print(float(5)) #converting int to float
+
+x=5.5
+print(int(x)) #converting float to int
+
+x=5
+print(complex(x)) #converting int to complex number
+
---
+++ Strings.py
@@ -0,0 +1,26 @@
+x="Hello World"
+print(len(x)) #length of the word
+
+txt="Hello World"
+print(txt[0]) #first character of string
+
+txt="Hello World"
+print(txt[2:5]) #output string between [2;5)
+
+txt="   Hello World "
+print(x.strip()) #eturn the string without any whitespace at the beginning or the end
+
+txt="   Hello World "
+print(txt.upper()) #Convert the value of txt to upper case
+
+txt="Hello World"
+print(txt.lower()) #Convert the value of txt to lower case
+
+txt="Hello World "
+print(txt.replace("H","J")) #replace characters int string
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age)) #placeholder for the age parametr
+
+
