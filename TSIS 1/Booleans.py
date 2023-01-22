64d70176a3ae50fd6a94ecfb1df0fbca3c2e0363
Sagingaly <123260920+Sagingaly@users.noreply.github.com>
2023-01-22 16:56:40 +0600

    Add files via upload

---
+++ TSIS 1/Booleans.py
@@ -0,0 +1,10 @@
+print(10 > 9) #output is True
+
+print(10==9) #output is False
+
+print(10 < 9) #output is False
+
+print(bool("abc")) #output is True
+
+print(bool(0)) #output is False
+
---
+++ TSIS 1/Variables.py
@@ -0,0 +1,27 @@
+carname= "Volvo"
+print(carname)
+
+x=50
+print(x)
+
+x=5
+y=10
+print(x+y)
+
+x=5
+y=10
+z=x+y
+print(z)
+
+#2my-first_name="John" # A variable cannot start with numbers and "-" does not contain such a character
+myfirst_name="John" #Removes 2 and "-".
+
+x=y=z="Orange"
+print(x)
+print(y)
+print(z)
+
+def myfunc():
+    global x
+   x= "fantastic"
+
