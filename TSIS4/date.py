7f5071c3e8f876c0f40e540deff455c164932ee9
Sagingaly <123260920+Sagingaly@users.noreply.github.com>
2023-02-18 20:47:48 +0600

    Update date.py

--- TSIS4/date.py
+++ TSIS4/date.py
@@ -1,30 +1,3 @@
-
-#print(datetime.date.today())
-#print(datetime.datetime(2022,10,26,10,30,21))
-#td_object=datetime.timedelta(days=5,seconds=3,weeks=5,microseconds=5,milliseconds=5,hours=2,minutes=3)
-#print(td_object)
-"""
-date=datetime.datetime.today()+datetime.timedelta(days=7)
-print(date)
-date=datetime.datetime.today()-datetime.timedelta(days=7)
-print(date)
-
-import datetime
-ds1='Friday,April 1,2022'
-ds2='4/1/22'
-ds3='04-01-2022'
-dt1=datetime.datetime.strptime(ds1,'%A,%B,%d,%Y')
-dt2=datetime.datetime.strptime(ds2,'%m/%d/%y')
-dt3=datetime.datetime.strptime(ds3,'%m-%d-%Y')
-print(dt1)
-print(dt2)
-print(dt3)
-
-import datetime
-date_1=datetime.date(2022,5,15)
-date_2=datetime.date(2022,5,6)
-print(date_1-date_2)
-
 1)import datetime
 date_5=datetime.date.today()-datetime.timedelta(days=5)
 print(date_5)
